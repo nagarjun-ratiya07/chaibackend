f55555289036cd7dc18fa3a7b5ef28cdbd88b029
Nagarjun <nagarjunratiya7777@gmail.com>
2025-01-06 22:47:51 +0530

    github data added

--- index.js
+++ index.js
@@ -5,6 +5,43 @@ const app = express()
 
 const port = 3000
 
+const githubData = {
+    "login": "hiteshchoudhary",
+    "id": 11613311,
+    "node_id": "MDQ6VXNlcjExNjEzMzEx",
+    "avatar_url": "https://avatars.githubusercontent.com/u/11613311?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/hiteshchoudhary",
+    "html_url": "https://github.com/hiteshchoudhary",
+    "followers_url": "https://api.github.com/users/hiteshchoudhary/followers",
+    "following_url": "https://api.github.com/users/hiteshchoudhary/following{/other_user}",
+    "gists_url": "https://api.github.com/users/hiteshchoudhary/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/hiteshchoudhary/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/hiteshchoudhary/subscriptions",
+    "organizations_url": "https://api.github.com/users/hiteshchoudhary/orgs",
+    "repos_url": "https://api.github.com/users/hiteshchoudhary/repos",
+    "events_url": "https://api.github.com/users/hiteshchoudhary/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/hiteshchoudhary/received_events",
+    "type": "User",
+    "user_view_type": "public",
+    "site_admin": false,
+    "name": "Hitesh Choudhary",
+    "company": null,
+    "blog": "https://www.youtube.com/c/HiteshChoudharydotcom",
+    "location": "India",
+    "email": null,
+    "hireable": null,
+    "bio": "I make coding videos on youtube and for courses. My youtube channel explains my work more. Check that out",
+    "twitter_username": "hiteshdotcom",
+    "public_repos": 101,
+    "public_gists": 5,
+    "followers": 36694,
+    "following": 0,
+    "created_at": "2015-03-23T13:03:25Z",
+    "updated_at": "2024-12-30T20:55:07Z"
+  }
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -18,6 +55,10 @@ app.get("/login" , (req,res) => {
 
 app.get('/youtube',(req,res)=>{
     res.send("<h2>Chai aur Code</h2>")
+})
+
+app.get('/github' , (req,res) =>{
+    res.json(githubData)
 })
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
